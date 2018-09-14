6740fb83145cebfa73890495a47ddc74ccf24ba4
oheydrew <drew@oheydrew.me>
2018-09-14 16:48:07 +1000

    Add Parser.parse tests

--- spec/toy_robot/parser_spec.rb
+++ spec/toy_robot/parser_spec.rb
@@ -4,4 +4,48 @@ RSpec.describe ToyRobot::Parser do
       expect(ToyRobot::Parser.test).to be(true)
     end
   end
+
+  describe 'parse (string input)' do
+    it 'can parse a MOVE command' do
+      expect(ToyRobot::Parser.parse('MOVE')).to eq(command: :move)
+    end
+
+    it 'can parse a PLACE command' do
+      expect(ToyRobot::Parser.parse('PLACE 2,2,NORTH'))
+        .to eq(
+          command: :move,
+          args: {
+            position: {
+              x: 2,
+              y: 2
+            },
+            facing: :north
+          }
+        )
+    end
+
+    it 'can parse a LEFT command' do
+      expect(ToyRobot::Parser.parse('LEFT'))
+        .to eq(
+          command: :turn,
+          args: {
+            direction: :left
+          }
+        )
+    end
+
+    it 'can parse a RIGHT command' do
+      expect(ToyRobot::Parser.parse('RIGHT'))
+        .to eq(
+          command: :turn,
+          args: {
+            direction: :right
+          }
+        )
+    end
+
+    it 'can parse a REPORT command' do
+      expect(ToyRobot::Parser.parse('REPORT')).to eq(command: :report)
+    end
+  end
 end
