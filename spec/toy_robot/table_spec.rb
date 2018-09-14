1b6fe30b866f4222c8c1cd17c40f063332fed538
oheydrew <drew@oheydrew.me>
2018-09-14 14:38:54 +1000

    Add Table.in_bounds?() basic tests

--- spec/toy_robot/table_spec.rb
+++ spec/toy_robot/table_spec.rb
@@ -5,5 +5,25 @@ RSpec.describe ToyRobot::Robot do
     it 'can instantiate an instance of Table' do
       expect(table).to be_an_instance_of(ToyRobot::Table)
     end
+
+    it 'can have a default size if none supplied' do
+      expect(table.size).to eq(x: 4, y: 4)
+    end
+
+    it 'can instantiate with size if supplied' do
+      table = ToyRobot::Table.new(x: 15, y: 15)
+      expect(table.size).to eq(x: 15, y: 15)
+    end
   end
+
+  describe '.in_bounds?' do
+    it 'returns true if in-bounds values supplied' do
+      expect(table.in_bounds?(x: 2, y: 2)).to be(true)
+    end
+
+    it 'returns false if out-of-bounds values supplied' do
+      expect(table.in_bounds?(x: 15, y: 15)).to be(false)
+    end
+  end
+
 end
