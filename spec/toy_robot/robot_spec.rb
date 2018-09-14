a231ee92247e49f171e6ff460dacfe6e290a63c7
oheydrew <drew@oheydrew.me>
2018-09-14 13:57:52 +1000

    Add more basic robot tests

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -6,4 +6,29 @@ RSpec.describe ToyRobot::Robot do
       expect(robot).to be_an_instance_of(ToyRobot::Robot)
     end
   end
+
+  describe 'position / move' do
+    it 'can return it\'s position' do
+      expect(robot.position).to eq(x: 0, y: 0)
+    end
+
+    it 'can move forward' do
+      robot.move
+      expect(robot.position).to eq(x: 0, y: 1)
+      expect(robot.facing).to eq(:north)
+    end
+  end
+
+  describe 'turning' do
+    it 'can turn left' do
+      robot.turn(:left)
+      expect(robot.facing).to eq(:east)
+    end
+
+    it 'can turn right' do
+      robot.turn(:right)
+      expect(robot.facing).to eq(:west)
+    end
+  end
+
 end
