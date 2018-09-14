c6feef71527a0aaa25713cd22882e44cf109a71a
oheydrew <drew@oheydrew.me>
2018-09-14 14:43:26 +1000

    Add Table.in_bounds?(), pass basic tests

--- lib/toy_robot/table.rb
+++ lib/toy_robot/table.rb
@@ -8,5 +8,12 @@ module ToyRobot
         y: y
       }
     end
+
+    def in_bounds?(x:, y:)
+      x_in_bounds = x >= 0 && x <= @size[:x]
+      y_in_bounds = y >= 0 && y <= @size[:x]
+
+      x_in_bounds && y_in_bounds
+    end
   end
 end
