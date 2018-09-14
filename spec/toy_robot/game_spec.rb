82a8b53046262ddbe1ae4b145cc0c51c277e06fe
oheydrew <drew@oheydrew.me>
2018-09-14 15:36:49 +1000

    Add Game.create_robot basic failing tests

--- spec/toy_robot/game_spec.rb
+++ spec/toy_robot/game_spec.rb
@@ -20,4 +20,33 @@ RSpec.describe ToyRobot::Game do
       expect(game.table.size).to eq(x: 4, y: 4)
     end
   end
+
+  describe 'robot creation and naming' do
+    it 'can create a new robot at position with default name' do
+      game.create_table
+      game.create_robot(x: 2, y: 2, facing: :north)
+      expect(game.robot).to be_an_instance_of(ToyRobot::Robot)
+      expect(game.robot.position).to eq(x: 2, y: 2)
+      expect(game.robot.facing).to eq(:north)
+    end
+
+    it 'can create a new robot at position with given name' do
+      game.create_table
+      game.create_robot(x: 2, y: 2, facing: :north)
+      expect(game.robot).to be_an_instance_of(ToyRobot::Robot)
+      expect(game.robot.position).to eq(x: 2, y: 2)
+      expect(game.robot.facing).to eq(:north)
+    end
+
+    it 'raises an error if no table is created' do
+      expect { game.create_robot(x: 2, y: 2, facing: :north) }
+        .to raise_error(RuntimeError)
+    end
+
+    it 'raises an error if placed out of bounds' do
+      game.create_table
+      expect { game.create_robot(x: 15, y: 15, facing: :north) }
+        .to raise_error(RuntimeError)
+    end
+  end
 end
