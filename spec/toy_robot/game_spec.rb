c380d231d27811e987d1ca728b8fff5d5b0ace7c
oheydrew <drew@oheydrew.me>
2018-09-14 15:14:39 +1000

    Add Game.create_table basic failing tests

--- spec/toy_robot/game_spec.rb
+++ spec/toy_robot/game_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe ToyRobot::Game do
       expect(game).to be_an_instance_of(ToyRobot::Game)
     end
   end
+
+  describe 'table related' do
+    it 'can create a new table with a size' do
+      game.create_table(x: 2, y: 2)
+      expect(game.table).to be_an_instance_of(ToyRubot::Table)
+      expect(game.table.size).to eq(x: 2, y: 2)
+    end
+
+    it 'can create a new table with no size' do
+      game.create_table
+      expect(game.table).to be_an_instance_of(ToyRubot::Table)
+      expect(game.table.size).to eq(x: 4, y: 4)
+    end
+  end
 end
